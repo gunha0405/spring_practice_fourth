fdb1a26176a3f481401d32c90093da2157938c3c
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-19 10:50:42 +0900

    [Feat] chapter 2-12 답변 기능 만들기

---
+++ src/main/java/com/example/answer/controller/AnswerController.java
@@ -0,0 +1,31 @@
+package com.example.answer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.answer.service.AnswerService;
+import com.example.question.model.Question;
+import com.example.question.service.QuestionService;
+
+import ch.qos.logback.core.model.Model;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+	
+	private final QuestionService questionService;
+	
+	private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Long id, @RequestParam(value="content") String content) {
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
