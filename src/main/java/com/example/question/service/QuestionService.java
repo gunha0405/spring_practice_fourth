095fad1dc073e05cb87cfc42552a68f117baf0ee
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-19 11:12:09 +0900

    [Feat] chapter 3-02 페이징 기능 추가하기

--- src/main/java/com/example/question/service/QuestionService.java
+++ src/main/java/com/example/question/service/QuestionService.java
@@ -1,9 +1,14 @@
 package com.example.question.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.exception.DataNotFoundException;
@@ -17,9 +22,12 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 	private final QuestionRepository questionRepository;
 	
-	public List<Question> getList() {
-		return this.questionRepository.findAll();
-	}
+	public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+    }
 	
 	public Question getQuestion(Long id) {  
         Optional<Question> question = this.questionRepository.findById(id);
