05f5cbbb1334fca27c0b9f08791fa4517427284a
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-19 09:15:28 +0900

    git init

---
+++ src/main/java/com/example/SpringPracticeFourthApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeFourthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeFourthApplication.class, args);
+	}
+
+}
