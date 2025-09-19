23870275386b2eebf02e1892ddadacd8e4e185ed
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-19 10:08:11 +0900

    [Feat] chapter 2-07, 2-11 질문 목록 만들기, URL 프리픽스 알아두기

---
+++ src/main/java/com/example/question/controller/QuestionController.java
@@ -0,0 +1,29 @@
+package com.example.question.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.question.model.Question;
+import com.example.question.respository.QuestionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/question")
+@RequiredArgsConstructor
+public class QuestionController {
+
+	private final QuestionRepository questionRepository;
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionList);
+		return "question_list";
+	}
+	
+}
