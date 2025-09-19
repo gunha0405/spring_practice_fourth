789ea9771b1dda4a6327a696edb4204453940272
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-19 10:46:48 +0900

    [Feat] chapter 2-10 상세 페이지 만들기

--- src/main/java/com/example/question/controller/QuestionController.java
+++ src/main/java/com/example/question/controller/QuestionController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.question.model.Question;
-import com.example.question.respository.QuestionRepository;
 import com.example.question.service.QuestionService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,5 +26,12 @@ public class QuestionController {
 		model.addAttribute("questionList", questionList);
 		return "question_list";
 	}
+	
+	@GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Long id) {
+		Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
 	
 }
--- src/main/java/com/example/question/service/QuestionService.java
+++ src/main/java/com/example/question/service/QuestionService.java
@@ -1,9 +1,11 @@
 package com.example.question.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.exception.DataNotFoundException;
 import com.example.question.model.Question;
 import com.example.question.respository.QuestionRepository;
 
@@ -17,4 +19,13 @@ public class QuestionService {
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
 	}
+	
+	public Question getQuestion(Long id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
 }
