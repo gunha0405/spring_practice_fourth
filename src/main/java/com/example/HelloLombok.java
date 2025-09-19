2fc9f24a39383bae2aa2548006e9d04ff6735fb9
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-19 09:29:24 +0900

    [Feat] chapter 2-01 스프링 부트 프로젝트의 구조 이해

---
+++ src/main/java/com/example/HelloLombok.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class HelloLombok { 
+    private final String hello; 
+    private final int lombok; 
+}
